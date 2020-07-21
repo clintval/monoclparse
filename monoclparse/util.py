c439c48272528c1f064d5cf64903d2cc6e0be9f9
Clint Valentine <valentine.clint@gmail.com>
2020-07-20 23:28:19 -0700

    Satisfy linter and type checker

--- monoclparse/util.py
+++ monoclparse/util.py
@@ -1,4 +1,7 @@
-def format_column_name(levels) -> str:
+from typing import List
+
+
+def format_column_name(levels: List[str]) -> str:
     """Take a Monocl multi-index and squash it down to a single-index."""
     prefix, level2, *_ = levels
     suffix = '' if 'Unnamed' in level2 else ' ' + level2
